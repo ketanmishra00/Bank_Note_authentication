849e7b81d2bef3ce1bd0940d20b4e654c3cd6229
ketanmishra00 <61942292+ketanmishra00@users.noreply.github.com>
2021-09-23 11:50:51 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask,request
+import pandas as pd
+import numpy as np
+import pickle
+
+
+app=Flask(__name__)      
+pickle_in=open('model.pkl','rb')
+model=pickle.load(pickle_in)
+
+@app.route('/')
+def welcome():
+    return"welcome to online bank note authentication centre"
+@app.route('/predict')
+def predict_note_authentication():
+    variance = request.args.get('variance')
+    skewness = request.args.get('skewness')
+    curtosis = request.args.get('curtosis')
+    entropy = request.args.get('entropy')
+    prediction=model.predict([[variance,skewness,curtosis,entropy]])
+    return"The predicted value is"+ str(prediction)
+
+
+
+
+ 
+
+
+if __name__ =='__main__':
+    app.run()
+     
